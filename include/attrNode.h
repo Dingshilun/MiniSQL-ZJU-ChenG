04ee7dbdbcab2d480300594f4faee11e37e94596
Dingshilun <zgrxdhp@qq.com>
2015-10-19 15:03:45 +0800

    indexNode需要凯文来完成

---
+++ include/attrNode.h
@@ -0,0 +1,12 @@
+#ifndef _attrNode_h_
+#define _attrNode_h_
+#include<string>
+#include"miniSQL.h"
+using namespace std;
+typedef struct AattrNode
+{
+    bool isPrimary,isUnique;
+    int length,type,offset;//type={MINI_FLOAT,MINI_INT,MINI_STRING}
+    string attrName;
+}attrNode;
+#endif
