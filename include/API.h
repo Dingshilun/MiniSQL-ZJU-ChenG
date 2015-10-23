4d25d5c5b4d712edf7271bb8b3c7be360b558845
Dingshilun <zgrxdhp@qq.com>
2015-10-23 19:25:04 +0800

    Added the head file of API

---
+++ include/API.h
@@ -0,0 +1,29 @@
+#ifndef _API_H_
+#define _API_H_
+#include"interface.h"
+#include"catalogManager.h"
+#include"IndexManager.h"
+#include"string"
+#include"iostream"
+class API
+{
+private:
+	catalogManager& catlog;
+	IndexManager *indexmanager;
+	/*recordmanager*/
+	int createTable(string tablename, list<attrNode> attributes);
+	int createIndex(string tablename, string indexname, int column);
+	int Insert(string tablename, list<attrNode> attlist);
+	int Delete(string tablename, vector<TreeNode> conditions);
+	int dropTable(string tablename);
+	int dropIndex(string indexname);
+public:
+	API(catalogManager&tmp) :catlog(tmp) {}
+	~API(){
+		if (indexmanager) delete indexmanager;
+	}
+	int exec(Interface inter);
+
+};
+
+#endif
