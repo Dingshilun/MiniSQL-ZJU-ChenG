8fcc7a2fc67aa0b4c1e2a0c30d9309519430b48d
Dingshilun <zgrxdhp@qq.com>
2015-10-24 00:14:06 +0800

    add a new struct attrAndvalue

--- include/API.h
+++ include/API.h
@@ -5,6 +5,14 @@
 #include"IndexManager.h"
 #include"string"
 #include"iostream"
+typedef struct aav
+{
+	bool isPrimary, isUnique;
+	int length, type;//type={MINI_FLOAT,MINI_INT,MINI_STRING}
+	string str_value;//if attribute is string
+	int int_value;//if attribute is int
+	float float_value;
+}attrAndvalue;
 class API
 {
 private:
@@ -13,7 +21,7 @@ private:
 	/*recordmanager*/
 	int createTable(string tablename, list<attrNode> attributes);
 	int createIndex(string tablename, string indexname, int column);
-	int Insert(string tablename, list<attrNode> attlist);
+	int Insert(string tablename, list<attrAndvalue> attlist);
 	int Delete(string tablename, vector<TreeNode> conditions);
 	int dropTable(string tablename);
 	int dropIndex(string indexname);
