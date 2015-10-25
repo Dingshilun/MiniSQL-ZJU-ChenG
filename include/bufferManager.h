7f8f8b3c95eab8d42c6e3c7df121a76150fa99f8
suntc <s3130103354@gmail.com>
2015-10-25 21:36:36 +0800

    bufferManager基本完成，开头的注释中有一些说明

--- include/bufferManager.h
+++ include/bufferManager.h
@@ -1,32 +1,74 @@
+/*
+pin为true的block在buffer满时不会被替换
+getBlock中的offset指第几个block
+其余offset(block内的)全部指字节
+length也是字节
+type:table=0,index=1
+*/
+
 #ifndef __BUFFERMANAGER__H
 #define __BUFFERMANAGER__H
 
+#define  BLOCK_SIZE 4096
+#define  MAX_BLOCK 1024
+
 #include <string>
+#include <vector>
+
+enum Type{
+	TABLE, INDEX
+};
 
 class bufferNode
 {
 public:
 	bufferNode();
-	~bufferNode();
+	virtual ~bufferNode();
 
-	void setBufferNode(int type, std::string filename, int offset);
+	void setBufferNode(int type, std::string filename, int offset);//type:table=0,index=1
 	void flush();
 	bool isMatch(int type, std::string filename, int offset);
 	bool isEmpty();
+	int age;
+	bool pin;
+	template<class T>
+	void writeBlock(T data, int length, int offset)
+	{
+		this->dirty = true;
+		memcpy(this->dataField + offset, (char*)&data, length);
+	}
+	void writeBlock(std::string data, int length, int offset);
+	template<class T>
+	void readBlock(T& dest, int length, int offset)
+	{
+		memcpy((char*)&dest, this->dataField + offset, length);
+	}
+	void readBlock(std::string& dest, int length, int offset);
 protected:
+	char* dataField;
 	std::string fileName;
 	int offset;
+	int type;
 	bool dirty;
-	bool pin;
+
 };
 
 class bufferManager
 {
+protected:
+
+	std::vector<bufferNode>* bufferPool;
+	int curAge;//for LRU
+	int minAge;
+	bool lruBase;
+	std::vector<bufferNode>::iterator minIndex;
 public:
 	bufferManager();
-	~bufferManager();
-
-	bufferNode getBlock(int type, std::string filename, int offset);
+	virtual ~bufferManager();
+	std::vector<bufferNode>::iterator LRU();
+	void flushAll();
+	void usingBlock(std::vector<bufferNode>::iterator bn_ite);
+	bufferNode& getBlock(int type, std::string filename, int offset);
 };
 
 #endif
