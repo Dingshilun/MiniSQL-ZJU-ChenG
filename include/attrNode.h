249364797933623c4c0a07a7d960eb096b2a45aa
Dingshilun <zgrxdhp@qq.com>
2015-10-29 00:09:37 +0800

    Revert "add some new function and properties"
    This reverts commit ae1add9842b58977725f1bae6e4885e572d99c36.

--- include/attrNode.h
+++ include/attrNode.h
@@ -5,7 +5,7 @@
 using namespace std;
 typedef struct AattrNode
 {
-    bool isPrimary,isUnique,hasIndex,isNull;
+    bool isPrimary,isUnique,hasIndex;
     int length,type,offset;//type={MINI_FLOAT,MINI_INT,MINI_STRING}
     string attrName;
 }attrNode;
