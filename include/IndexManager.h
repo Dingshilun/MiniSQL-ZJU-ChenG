b43510d65819e4609e7db7fc60dacd410baf0df2
Dingshilun <zgrxdhp@qq.com>
2015-10-29 20:43:32 +0800

    addition to indexmanager

--- include/IndexManager.h
+++ include/IndexManager.h
@@ -2,10 +2,14 @@
 #define _INDEXMANAGE_H_
 #include <vector>
 #include <map>
+#include "recordManager.h"
 using namespace std;
 
-#define SEARCHSINGLE 0
-#define
+#define SINGLE 0
+#define LARGER 1
+#define NOTLESS 2
+#define NOTLARGER 3
+#define LESS 4
 
 class IndexManager
 {
@@ -39,14 +43,12 @@ class Target
 private:
 	string table;
 	string attribute;
-	T key,value;
-	T beginKey;
-	T endKey;
-	bool isSingle;
+	T key;
+	index_info index;
+	int type;
 public:
 	Target( string t, string a )
 	Target( string t, string a, T x );
-	Target( string t, string a, T x, T y );
 	~Target();
 	void setTable( string name );
 	void setAttribute( string name );
@@ -54,6 +56,7 @@ public:
 	void setEndKey( T x );
 	void setKey(T x);
 	void setvalue(T x);
+	void setindex_info(index_info tmp);
 	string getTable();
 	strinng getAttribute();
 	T getKey();
@@ -82,7 +85,7 @@ public:
 
 template < class T >
 class InteriorNode
-{
+{ 
 private:
 	int M;  
 	vector<T> key;
@@ -101,7 +104,7 @@ public:
 
 template< class T >
 class LeafNode:public InteriorNode
-{
+{ 
 private:
 	vector<int> blockNumber;
 	vector<int> offset;
@@ -109,8 +112,8 @@ private:
 
 public:
 	virtual bool isEmpty();
-	??? getRecordAddress( T target );
-    LeafNode* getNextLeaf();
+	vector<index_info> getRecordAddress( T target );
+	LeafNode* getNextLeaf();
 };
 
 #endif
