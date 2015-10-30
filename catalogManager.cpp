16007295b7e25c95fd6ef6c5bf2520d296329b67
suntc <s3130103354@gmail.com>
2015-10-30 17:03:14 +0800

    catalog 头文件与原先有修改，其中indexNode在整合时可能要重新包含

---
+++ catalogManager.cpp
@@ -0,0 +1,396 @@
+#include "catalogManager.h"
+#include <fstream>
+#include <iostream>
+#include <exception>
+
+using namespace std;
+catalogManager::catalogManager()
+{
+}
+
+
+catalogManager::~catalogManager()
+{
+}
+
+void catalogManager::readCatalog()
+{
+	fstream logfile;
+	logfile.open("tableLog.log", ios::in);
+	if (logfile.good())
+	{
+		string s;
+		logfile >> s;
+		if (s.length() != 0)
+		{
+			logfile.seekg(ios::beg);
+			try
+			{
+				copy(istream_iterator<TableNode>(logfile), istream_iterator<TableNode>(), back_inserter(this->tableList));
+
+			}
+			catch (const length_error& e)
+			{
+				cerr << e.what() << endl;
+			}
+
+			logfile.close();
+		}
+		
+	}
+	logfile.open("indexLog.log", ios::in);
+	if (logfile.good())
+	{
+		copy(istream_iterator<indexNode>(logfile), istream_iterator<indexNode>(), back_inserter(this->indexList));
+		logfile.close();
+	}
+}
+
+void catalogManager::writeCatalog()
+{
+	fstream logfile;
+	logfile.open("tableLog.log", ios::out);
+	if (logfile.good())
+	{
+		copy(this->tableList.begin(), this->tableList.end(), ostream_iterator<TableNode>(logfile));
+		logfile.close();
+	}
+	logfile.open("indexLog.log", ios::out);
+	if (logfile.good())
+	{
+		copy(this->indexList.begin(), this->indexList.end(), ostream_iterator<indexNode>(logfile));
+		logfile.close();
+	}
+}
+
+bool catalogManager::createTable(string tablename, vector<attrNode> attrlist)
+{
+	fstream tablefile;
+	tablefile.open(tablename + ".tbl", ios::in);
+	if (tablefile.good())//already exists
+	{
+		return false;
+	}
+	else
+	{
+		tablefile.close();
+		tablefile.open(tablename + ".tbl", ios::out);
+		tablefile.close();
+		TableNode node(tablename, attrlist, 0);
+		this->tableList.push_back(node);
+		return true;
+	}
+}
+
+bool catalogManager::createIndex(string indexname, string tablename, int columns)
+{
+	fstream indexfile;
+	indexfile.open(indexname + "-" + tablename + ".idx", ios::in);
+	if (indexfile.good())//already exists
+	{
+		return false;
+	}
+	else
+	{
+		indexfile.close();
+		indexfile.open(indexname + "-" + tablename + ".idx", ios::out);
+		indexfile.close();
+		//find attrName of column
+		string attrname;
+		vector<TableNode>::iterator tite;
+		vector<AattrNode>::iterator aite;
+		for (tite = this->tableList.begin(); tite != this->tableList.end(); ++tite)
+		{
+			if (tite->tableName == tablename)
+			{
+				attrname = tite->attrList[columns].attrName;
+				break;
+			}
+		}
+
+		indexNode node(tablename, indexname, attrname);
+		this->indexList.push_back(node);
+		return true;
+	}
+}
+
+bool catalogManager::doesTableExist(string tablename)
+{
+	vector<TableNode>::iterator tite;
+	for (tite = this->tableList.begin(); tite != this->tableList.end(); ++tite)
+	{
+		if (tite->tableName == tablename)
+		{
+			return true;
+		}
+	}
+	return false;
+}
+
+bool catalogManager::doesIndexExist(string indexname, string tablename)
+{
+	vector<indexNode>::iterator iite;
+	for (iite = this->indexList.begin(); iite != this->indexList.end(); ++iite)
+	{
+		if (iite->indexName == indexname&&iite->tableName == tablename)
+			return true;
+	}
+	return false;
+}
+
+indexNode catalogManager::findindex(string indexname,string tablename)
+{
+	vector<indexNode>::iterator iite;
+	for (iite = this->indexList.begin(); iite != this->indexList.end(); ++iite)
+	{
+		if (iite->indexName == indexname&&iite->tableName == tablename)
+			return *iite;
+	}
+}
+
+bool catalogManager::doesAttrExist(string tablename, string attrname)
+{
+	vector<TableNode>::iterator tite;
+	vector<AattrNode>::iterator aite;
+	for (tite = this->tableList.begin(); tite != this->tableList.end(); ++tite)
+	{
+		if (tite->tableName == tablename)
+		{
+			for (aite = tite->attrList.begin(); aite != tite->attrList.end(); ++aite)
+			{
+				if (aite->attrName == attrname)
+					return true;
+			}
+		}	
+	}
+	return false;
+}
+
+bool catalogManager::isAttrUnique(string tablename, string attrname)
+{
+	vector<TableNode>::iterator tite;
+	vector<AattrNode>::iterator aite;
+	for (tite = this->tableList.begin(); tite != this->tableList.end(); ++tite)
+	{
+		if (tite->tableName == tablename)
+		{
+			for (aite = tite->attrList.begin(); aite != tite->attrList.end(); ++aite)
+			{
+				if (aite->isUnique)
+					return true;
+				else
+					return false;
+			}
+		}
+	}
+	return -1;//not found
+}
+
+int catalogManager::getAttrType(string tablename, string attrname)
+{
+	vector<TableNode>::iterator tite;
+	vector<AattrNode>::iterator aite;
+	for (tite = this->tableList.begin(); tite != this->tableList.end(); ++tite)
+	{
+		if (tite->tableName == tablename)
+		{
+			for (aite = tite->attrList.begin(); aite != tite->attrList.end(); ++aite)
+			{
+				if (aite->attrName == attrname)
+					return aite->type;
+			}
+		}
+	}
+	return -1;//not found
+}
+
+int catalogManager::getAttrOffset(string tablename, string attrname)
+{
+	vector<TableNode>::iterator tite;
+	vector<AattrNode>::iterator aite;
+	for (tite = this->tableList.begin(); tite != this->tableList.end(); ++tite)
+	{
+		if (tite->tableName == tablename)
+		{
+			for (aite = tite->attrList.begin(); aite != tite->attrList.end(); ++aite)
+			{
+				if (aite->attrName == attrname)
+					return aite->offset;
+			}
+		}
+	}
+	return -1;//not found
+}
+int catalogManager::getAttrLength(string tablename, string attrname)
+{
+	vector<TableNode>::iterator tite;
+	vector<AattrNode>::iterator aite;
+	for (tite = this->tableList.begin(); tite != this->tableList.end(); ++tite)
+	{
+		if (tite->tableName == tablename)
+		{
+			for (aite = tite->attrList.begin(); aite != tite->attrList.end(); ++aite)
+			{
+				if (aite->attrName == attrname)
+					return aite->length;
+			}
+		}
+	}
+	return -1;//not found
+}
+
+vector<attrNode> catalogManager::getAttrList(string tablename)
+{
+	vector<TableNode>::iterator tite;
+	vector<AattrNode>::iterator aite;
+	for (tite = this->tableList.begin(); tite != this->tableList.end(); ++tite)
+	{
+		if (tite->tableName == tablename)
+		{
+			return tite->attrList;
+		}
+	}
+
+}
+
+attrNode catalogManager::getAttrInfo(string tablename, string attrname)
+{
+	vector<TableNode>::iterator tite;
+	vector<AattrNode>::iterator aite;
+	for (tite = this->tableList.begin(); tite != this->tableList.end(); ++tite)
+	{
+		if (tite->tableName == tablename)
+		{
+			for (aite = tite->attrList.begin(); aite != tite->attrList.end(); ++aite)
+			{
+				if (aite->attrName == attrname)
+					return *aite;
+			}
+		}
+	}
+}
+
+vector<string> catalogManager::getIndexOfTable(string tablename)
+{
+	vector<indexNode>::iterator iite;
+	vector<string> sv;
+	for (iite = this->indexList.begin(); iite != this->indexList.end(); ++iite)
+	{
+		if (iite->tableName == tablename)
+			sv.push_back(iite->indexName);
+	}
+	return sv;
+}
+
+int catalogManager::getRecordNum(string tablename)//返回条目数量
+{
+	vector<TableNode>::iterator tite;
+	for (tite = this->tableList.begin(); tite != this->tableList.end(); ++tite)
+	{
+		if (tite->tableName == tablename)
+		{
+			return tite->recordNum;
+		}
+	}
+	return -1;
+}
+
+int catalogManager::getAttrNum(string tablename, string attrname)
+{
+	vector<TableNode>::iterator tite;
+	vector<AattrNode>::iterator aite;
+	int num = 0;
+	for (tite = this->tableList.begin(); tite != this->tableList.end(); ++tite)
+	{
+		if (tite->tableName == tablename)
+		{
+			for (aite = tite->attrList.begin(); aite != tite->attrList.end(); ++aite)
+			{
+				if (aite->attrName == attrname)
+					return num;
+				++num;
+			}
+		}
+	}
+	return -1;//not found;
+}
+
+bool catalogManager::deleteIndex(string indexname, string tablename)
+{
+	vector<indexNode>::iterator iite;
+	for (iite = this->indexList.begin(); iite != this->indexList.end();)
+	{
+		if (iite->tableName == tablename&&iite->attribute == indexname)
+		{
+			iite = this->indexList.erase(iite);
+			string filename = indexname + "-" + tablename + ".idx";
+			remove(filename.c_str());
+		}
+		else
+		{
+			++iite;
+		}
+		
+	}
+	return true;
+}
+
+bool catalogManager::deleteTable(string tablename)
+{
+	vector<TableNode>::iterator tite;
+	for (tite = this->tableList.begin(); tite != this->tableList.end(); )
+	{
+		if (tite->tableName == tablename)
+		{
+			tite = this->tableList.erase(tite);
+			string filename = tablename + ".tbl";
+			remove(tablename.c_str());
+		}
+		else
+		{
+			++tite;
+		}
+	}
+	vector<indexNode>::iterator iite;
+	for (iite = this->indexList.begin(); iite != this->indexList.end();)
+	{
+		if (iite->tableName == tablename)
+		{
+			string filename = iite->attribute + "-" + tablename + ".idx";
+			remove(filename.c_str());
+			this->indexList.erase(iite);
+		}
+		else
+		{
+			++iite;
+		}
+	}
+	return true;
+}
+
+bool catalogManager::recordDelete(string tablename, int num)//删除条目数量，删除成功返回1
+{
+	vector<TableNode>::iterator tite;
+	for (tite = this->tableList.begin(); tite != this->tableList.end(); ++tite)
+	{
+		if (tite->tableName == tablename)
+		{
+			tite->recordNum -= num;
+			return true;
+		}
+	}
+	return -1;//not found;
+}
+bool catalogManager::recordAdd(string tablename, int num)
+{
+	vector<TableNode>::iterator tite;
+	for (tite = this->tableList.begin(); tite != this->tableList.end(); ++tite)
+	{
+		if (tite->tableName == tablename)
+		{
+			tite->recordNum += num;
+			return true;
+		}
+	}
+	return -1;//not found;
+}
--- include/catalogManager.h
+++ include/catalogManager.h
@@ -1,40 +1,125 @@
 #ifndef _catalogManager_h_
 #define _catalogManager_h_
 #include<attrNode.h>
-#include<indexNode.h>
-#include<list>
-using namespace std;
+//#include<indexNode.h>
+
+#include <string>
+#include <vector>
+
+
+class indexNode
+{
+public:
+	indexNode()
+	{
+
+	}
+	indexNode(std::string tablename,std::string indexname,std::string attrname)
+	{
+		this->tableName = tablename;
+		this->indexName = indexname;
+		this->attribute = attrname;
+	}
+
+	std::string tableName;
+	std::string attribute;
+	std::string indexName;
+
+	friend std::ostream& operator<<(std::ostream& out, const indexNode& node)
+	{
+		out << node.tableName << '\t' << node.indexName << '\t' << node.attribute;
+		return out;
+	}
+
+	friend std::istream& operator>>(std::istream& in, indexNode& node)
+	{
+		in >> node.tableName >> node.indexName >> node.attribute;
+		return in;
+	}
+};
+
+class TableNode
+{
+public:
+
+	TableNode()
+	{
+		this->recordNum = 0;
+	}
+	TableNode(std::string tablename, std::vector<attrNode> attrlist, int recordnum)
+	{
+		this->tableName = tablename;
+		this->attrList = attrlist;
+		this->recordNum = recordnum;
+	}
+
+	std::string tableName;
+	std::vector<attrNode> attrList;
+	int recordNum;
+	friend std::ostream& operator<<(std::ostream& out, const TableNode& node)
+	{
+		out << node.tableName << '\t' << node.recordNum << '\t' << node.attrList.size() << endl;
+		for (vector<attrNode>::const_iterator aite = node.attrList.begin(); aite != node.attrList.end(); ++aite)
+		{
+			out << aite->isPrimary << '\t' << aite->isUnique << '\t' << aite->hasIndex << '\t' << aite->length << '\t' << aite->type << '\t' << aite->offset << '\t' << aite->attrName << endl;
+		}
+		return out;
+	}
+
+	friend std::istream& operator>>(std::istream& in, TableNode& node)
+	{
+		in >> node.tableName >> node.recordNum;
+		int s;
+		in >> s;
+		if (s < 0)
+			return in;
+		node.attrList.resize(s);
+		for (std::vector<attrNode>::iterator aite = node.attrList.begin(); aite != node.attrList.end(); ++aite)
+		{
+			in >> aite->isPrimary >> aite->isUnique >> aite->hasIndex >> aite->length >> aite->type >> aite->offset >> aite->attrName;
+		}
+		return in;
+	}
+};
+
 class catalogManager
 {
-    private 
+private:
+	std::vector<TableNode> tableList;
+	std::vector<indexNode> indexList;
+public:
+	//构造函数	
+	catalogManager();
+	virtual ~catalogManager();
+	void readCatalog();
+	void writeCatalog();
 	
-    public
-    //构造函数	
-    catalogManager();
+	//需要的接口
+	//1成功,0失败(返回bool)，-1不存在(如果返回值int)
+	bool createTable(string tablename, vector<attrNode> attrlist);
 	
-    //需要的接口
-    //1成功,0失败(返回bool)，-1不存在(如果返回值int)
-    bool createTable(string tablename,vector<attrNode> attrlist);
-    bool createIndex(string indexname,string tablename,int columns);//columns:第几列的属性需要index
-    bool doesTableExist(string tablename);
-    bool doesIndexExist(string indexname);
-    indexNode findindex(string indexname);
-    bool doesAttrExist(string tablename,string attrname);
-    bool isAttrUnique(string tablename,string attrname);
-    int getAttrType(string tablename,string attrname);
-    int getAttrOffset(string tablename,string attrname);
-    int getAttrLength(string tablename,string attrname);
-    int getAttrNum(string tablename,string attrname);
-    vector<attrNode> getAttrList(string tablename);
-    attrNode getAttrInfo(string tablename,string attrname);
-    vector<string> getIndexOfTable(string tablename);
-    string getIndexByAttrID(string tablename,int column);
-
-    int getRecordNum(string tablename);//返回条目数量
-    bool deleteTable(string tablename);
-    bool deleteIndex(string indexname,string tablename);
-    bool recordDelete(string tablename,int num);//删除条目数量，删除成功返回1
-    bool recordAdd(string tablename,int num);
+	bool createIndex(string indexname, string tablename, int columns);//columns:第几列的属性需要index
+	
+	bool doesTableExist(string tablename);
+	bool doesIndexExist(string indexname, string tablename);
+	indexNode findindex(string indexname,string tablename);
+	bool doesAttrExist(string tablename, string attrname);
+	bool isAttrUnique(string tablename, string attrname);
+	int getAttrType(string tablename, string attrname);
+	int getAttrOffset(string tablename, string attrname);
+	int getAttrLength(string tablename, string attrname);
+	int getAttrNum(string tablename, string attrname);
+	vector<attrNode> getAttrList(string tablename);
+	attrNode getAttrInfo(string tablename, string attrname);
+	vector<string> getIndexOfTable(string tablename);
+	int getRecordNum(string tablename);//返回条目数量
+	bool deleteTable(string tablename);
+	bool deleteIndex(string indexname, string tablename);
+	bool recordDelete(string tablename, int num);//删除条目数量，删除成功返回1
+	bool recordAdd(string tablename, int num);
 
 };
+
+
+
 #endif
